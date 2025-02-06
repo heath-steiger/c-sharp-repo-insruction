cf5b9eb147538e4ae17f5bfd6daf63ded9c453b7
heath-steiger <heathsteiger@gmail.com>
2025-02-06 09:27:00 -0500

    init commit

---
+++ 4-2-ex/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _4_2_ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbr;
+            PrintL("Welcome to the Factorial Calculator");
+            string choice = "y";
+            while (choice == "y") {
+                while (true) {
+
+                    string input = (GetString("\nEnter an integer that's greater than 0 and less than 10:"));
+                        if (int.TryParse(input, out nbr) && nbr > 0 && nbr < 16) {
+                            break; // Valid input, exit loop
+                        }
+                        PrintL("Invalid input. Please enter a number between 1 and 10.");
+                    
+                }
+                int F = 1;
+                for (int i = 1; i <= nbr; i++) {
+                    F *= i;
+                }
+                PrintL("The factorial of " + nbr + " is " + F);
+                choice = GetString("continue? (y/n)");
+            }
+            PrintL("Bye");
+
+        }
+        static string GetString(string prompt)
+        {
+            Print(prompt, "");
+            return Console.ReadLine();
+        }
+        private static void PrintL(string prompt, string endLine = "\n")
+        {
+            Console.WriteLine(prompt);
+        }
+        private static void Print(string prompt, string endLine = "\n")
+        {
+            Console.Write(prompt);
+        }
+
+    }
+}
---
+++ CH07-exceptions/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CH07_exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, chapter 7 - exceptions and data validation");
+
+            int nbr1 = 0;
+            while (true) {
+                Console.Write("Enter an interger: ");
+                if (!Int32.TryParse(Console.ReadLine(), out nbr1)) {
+                    Console.WriteLine("Error- enter whole number.");
+                }
+                else { break; }
+            }
+
+
+            Console.WriteLine("You entered: " + nbr1);
+
+
+
+            Console.WriteLine("bye");
+        }
+    }
+}
