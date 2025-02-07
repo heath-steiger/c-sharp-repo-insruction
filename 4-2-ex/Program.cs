12d2ee6fa90f5399f5e7a097ae17d7312985bfbb
heath-steiger <heathsteiger@gmail.com>
2025-02-07 09:34:27 -0500

    added validation

--- 4-2-ex/Program.cs
+++ 4-2-ex/Program.cs
@@ -5,41 +5,34 @@
         static void Main(string[] args)
         {
             int nbr;
-            PrintL("Welcome to the Factorial Calculator");
+            Print("Welcome to the Factorial Calculator");
             string choice = "y";
             while (choice == "y") {
                 while (true) {
-
                     string input = (GetString("\nEnter an integer that's greater than 0 and less than 10:"));
-                        if (int.TryParse(input, out nbr) && nbr > 0 && nbr < 16) {
-                            break; // Valid input, exit loop
-                        }
-                        PrintL("Invalid input. Please enter a number between 1 and 10.");
-                    
+                    if (int.TryParse(input, out nbr) && nbr > 0 && nbr < 16) {
+                        break; 
+                    }
+                    Print("\nInvalid input. Please enter a number between 1 and 10.");
                 }
                 int F = 1;
                 for (int i = 1; i <= nbr; i++) {
                     F *= i;
                 }
-                PrintL("The factorial of " + nbr + " is " + F);
+                Print("\nThe factorial of " + nbr + " is " + F + ".");
                 choice = GetString("continue? (y/n)");
             }
-            PrintL("Bye");
-
+            Print("Bye");
         }
         static string GetString(string prompt)
         {
-            Print(prompt, "");
+            Console.Write(prompt, "");
             return Console.ReadLine();
-        }
-        private static void PrintL(string prompt, string endLine = "\n")
-        {
-            Console.WriteLine(prompt);
         }
         private static void Print(string prompt, string endLine = "\n")
         {
-            Console.Write(prompt);
+            Console.WriteLine(prompt);
         }
-
+        
     }
 }
