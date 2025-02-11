d86cc2f6b6bd13cec83adc82d2e835a1ce3a0b03
heath-steiger <heathsteiger@gmail.com>
2025-02-10 21:16:46 -0500

    added restraints

--- ch08-1-ex/Program.cs
+++ ch08-1-ex/Program.cs
@@ -5,17 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the batting Average Calculator");
-
             string choice = "y";
             while (choice == "y") {
-                int bats = GetBats("\nEnter number of times at bat: ");
+                int bats = GetInt("\nEnter number of times at bat: ", 1, 30);
                 int[] atBat = new int[bats];
-                int t = 1;
                 int hits = 0;
                 Console.WriteLine("0 = out, 1 = single, 2 = double, 3 = triple, 4 = homerun");
                 Console.WriteLine("");
                 for (int i = 0; i < atBat.Length; i++) {
-                    atBat[i] = GetBases("Result for at-bat  " + t++ + ": ");
+                    atBat[i] = GetInt("Result for at-bat  " + (i+1) + ": ", 0, 4);
                 }
                 foreach (int result in atBat) {
                     if (result > 0) {
@@ -24,59 +22,35 @@
                 }
                 double batAve = (double)hits / atBat.Length;
                 double sum = atBat.Sum();
-                Console.WriteLine("\nBatting Average: " + batAve.ToString("0.000"));
-                Console.WriteLine("Slugging percent: " + (sum / bats));
+                Console.WriteLine("\nBatting Average: " + batAve.ToString(("f3")));
+                Console.WriteLine("Slugging percent: " + (sum / bats).ToString("f3"));
                 Console.Write("\nAnother player? (y/n)");
-                choice = Console.ReadLine();
+                choice = Console.ReadLine().ToLower();
             }
 
             Console.WriteLine("bye");
         }
-        static int GetBats(string prompt)
+        static int GetInt(string prompt, int min, int max)
         {
             int nbr = 0;
             bool success = false;
             while (!success) {
                 Console.Write(prompt);
-                try {
-                    nbr = Int32.Parse(Console.ReadLine());
-                }
-                catch (Exception e) {
-                    Console.WriteLine("Error - entry must be a whole number.");
-                    continue;
-                }
-                if (nbr >= 1 && nbr <= 30) {
-                    success = true;
+                if (Int32.TryParse(Console.ReadLine(), out nbr)) {
+                    if (nbr >= min && nbr <= max) {
+                        success = true;
+                    }
+                    else {
+                        Console.WriteLine("Error - entry must be between " + min + " and " + max);
+                    }
                 }
                 else {
-                    Console.WriteLine("Error - entry must be between " + 1 + " and " + 30);
+                    Console.WriteLine("Error - entry must be between an integer.");
                 }
             }
             return nbr;
         }
-        static int GetBases(string prompt)
-        {
-            int nbr = 0;
-            bool success = false;
-            while (!success) {
-                Console.Write(prompt);
-                try {
-                    nbr = Int32.Parse(Console.ReadLine());
-                }
-                catch (Exception e) {
-                    Console.WriteLine("Error - entry must be a whole number.");
-                    continue;
-                }
-                if (nbr >= 0 && nbr <= 4) {
-                    success = true;
-                }
-                else {
-                    Console.WriteLine("Error - entry must be between " + 0 + " and " + 4);
-                }
-            }
-            return nbr;
-        }
-
+    
 
     }
 }
--- ch08-2-ex/Program.cs
+++ ch08-2-ex/Program.cs
@@ -4,47 +4,72 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The Sales Report application");
+            PrintL("The Sales Report application");
 
 
-            var columnNames = new List<string> { "Q1", "\tQ2", "\tQ3", "\tQ4" };
-            var rowNames = new List<string> {  "1", "2", "3", "4" };
+
             
-            decimal[,] sales = new decimal[4, 4]
-            {
-            { 1540.00m, 2010.00m, 2450.00m, 1845.00m },
-            { 1130.00m, 1168.00m, 1847.00m, 1491.00m },
-            { 1580.00m, 2305.00m, 2710.00m, 1284.00m },
-            { 1105.00m, 4102.00m, 2391.00m, 1576.00m }
+            decimal sum = 0.0m;
+            decimal[,] sales =  {
+                    { 1540.00m, 2010.00m, 2450.00m, 1845.00m },
+                    { 1130.00m, 1168.00m, 1847.00m, 1491.00m },
+                    { 1580.00m, 2305.00m, 2710.00m, 1284.00m },
+                    { 1105.00m, 4102.00m, 2391.00m, 1576.00m }
             };
 
-           
-            Console.Write("Region\t");
-            foreach (var column in columnNames) {
-                Console.Write($"{column}\t");
-            }
-            Console.WriteLine(); 
-            for (int i = 0; i < sales.GetLength(0); i++)
-            {
-                Console.Write($"{rowNames[i]}\t"); 
-
-                for (int j = 0; j < sales.GetLength(1); j++) 
-                {
-                    Console.Write($"${sales[i, j]:N2}\t"); 
+            // step one dump data to console by region and quarter
+            PrintL("Region\tQ1\t\tQ2\t\tQ3\t\tQ4");
+            for (int i = 0; i < sales.GetLength(0); i++) {
+                Print((i + 1) + "\t"); 
+                for (int j = 0; j < sales.GetLength(1); j++) {
+                    Print(sales[i,j].ToString("c") + "\t");
                 }
-                Console.WriteLine();
+                PrintL("");
             }
+            PrintL("");
+                // sum sales by region
+            PrintL("Sales by region: ");
+            for (int i = 0; i < sales.GetLength(0); i++) {
+                Print("Region " +(i + 1) + ": ");
+                sum = 0.0m;
+                for (int j = 0; j < sales.GetLength(1); j++) {
+                    sum += sales[i, j];
+                }
+                PrintL(sum.ToString("c"));
+            }
+            PrintL(" ");
+            // sum sales by quarter
+            PrintL("Sales by quarter: ");
+            for (int i = 0; i < sales.GetLength(0); i++) {
+                Print("Q" + (i + 1) + ": ");
+                sum = 0.0m;
+                for (int j = 0; j < sales.GetLength(1); j++) {
+                    sum += sales[j, i];
+                }
+                PrintL(sum.ToString("c"));
+            }
+            PrintL(" ");
+            // total sales
+            sum = 0.0m;
+            Print("Total Sales: ");
+            for (int i = 0; i < sales.GetLength(0); i++) {
+                for (int j = 0; j < sales.GetLength(1); j++) {
+                    sum += sales[i, j];
+                }
+            }
+            PrintL(sum.ToString("c"));
+            PrintL("\nbye");
+        }
+        
+        static void PrintL(string message)
+        {
+            Console.WriteLine(message);
 
-            
+        }
+        static void Print(string message)
+        {
+            Console.Write(message);
 
-
-
-
-
-
-
-
-                Console.WriteLine("bye");
         }
     }
 }
