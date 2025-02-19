dec01319b514752537f0b8d4b770f287d6101fce
heath-steiger <heathsteiger@gmail.com>
2025-02-19 16:23:21 -0500

    init commit

---
+++ Ch09HangmanDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ch09HangmanDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
